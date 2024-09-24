989c036824cc1b7db74790423e40c5939da4c06e
mjchoo <mjchoo97@gmail.com>
2024-09-24 14:38:09 +0800

    docker commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Navbar from "./components/Navbar";
+import Navbar from "../components/Navbar";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,15 +26,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <div className="w-full px-4 md:px-8 lg:px-16 xl:px-32">
-          <Navbar />
-          {children}
-        </div>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        >
+          <div className="w-full px-4 md:px-8 lg:px-16 xl:px-32">
+            <Navbar />
+            {children}
+          </div>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between py-4 items-center">
+      <div>
+        <Link href="/">MovieBuzz</Link>{" "}
+      </div>
+      <div className="flex gap-2">
+        <Link href="/addmovie">Create</Link>
+        <Link href="/browsemovie">Browse</Link>
+        <div>
+          <ClerkLoading>
+            <div
+              className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+              role="status"
+            >
+              <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]">
+                Loading...
+              </span>
+            </div>
+          </ClerkLoading>
+          <ClerkLoaded>
+            <SignedIn>
+              <div className="flex items-center gap-3">
+                <UserButton />
+              </div>
+            </SignedIn>
+            <SignedOut>
+              <div className="cursor-pointer">
+                <Link href="/sign-in">Login/Register</Link>
+              </div>
+            </SignedOut>
+          </ClerkLoaded>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
